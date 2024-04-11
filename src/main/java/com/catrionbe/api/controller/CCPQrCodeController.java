7d658dcadf2fb3c0dce40c47f31fd611ea0e7444
Mani <mani.r@fourbends.com>
2024-04-11 11:10:26 +0530

    QRCode

--- src/main/java/com/catrionbe/api/controller/CCPQrCodeController.java
+++ src/main/java/com/catrionbe/api/controller/CCPQrCodeController.java
@@ -8,11 +8,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.catrionbe.api.model.CourseUpdateRequest;
+import com.catrionbe.api.model.QRCodeRequest;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
@@ -22,13 +25,17 @@ import com.google.zxing.qrcode.QRCodeWriter;
 @RequestMapping("/qr")
 public class CCPQrCodeController {
 
- @GetMapping(value = "/{barcode}", produces = MediaType.IMAGE_PNG_VALUE)
- public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@PathVariable("barcode") String barcode)
+ @GetMapping(value = "/GenerateQRCode", produces = MediaType.IMAGE_PNG_VALUE)
+ //public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@PathVariable("barcode") String barcode)
+ public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@RequestBody QRCodeRequest  qrcodeReq)
+
+ 
+ 
    throws Exception {
 
   QRCodeWriter barcodeWriter = new QRCodeWriter();
      BitMatrix bitMatrix = 
-       barcodeWriter.encode(barcode, BarcodeFormat.QR_CODE, 200, 200);
+       barcodeWriter.encode(qrcodeReq.getQrCodeUrl(), BarcodeFormat.QR_CODE, 200, 200);
 
   return new ResponseEntity<>(MatrixToImageWriter.toBufferedImage(bitMatrix),HttpStatus.OK);
  }
